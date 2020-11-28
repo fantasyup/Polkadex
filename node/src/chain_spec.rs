0a3915bb6f3529965ea7fcecbb4a15ebac167f6b
Gautham J <Gauthamastro@gmail.com>
2020-11-28 23:21:27 +0530

    Finality is lagging, hence removed validators

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -108,9 +108,9 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 				authority_keys_from_seed("Alice"),
 				authority_keys_from_seed("Bob"),
 				authority_keys_from_seed("Charlie"),
-				authority_keys_from_seed("Dave"),
-				authority_keys_from_seed("Eve"),
-				authority_keys_from_seed("Ferdie"),
+				// authority_keys_from_seed("Dave"),
+				// authority_keys_from_seed("Eve"),
+				// authority_keys_from_seed("Ferdie"),
 			],
 			// Sudo account
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
