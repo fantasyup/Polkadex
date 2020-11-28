2d2e68d68f696c37ebe5eb56368c61a05a6918a7
Gautham J <Gauthamastro@gmail.com>
2020-11-28 23:42:13 +0530

    Added peerset debug

--- scripts/validator_bob.sh
+++ scripts/validator_bob.sh
@@ -1,3 +1,3 @@
 rm -r -f /tmp/bob/
-../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --bob --port 30333 --ws-port 9945 --rpc-port 9934 --validator --node-key 0000000000000000000000000000000000000000000000000000000000000002  --execution Native \
+../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --bob --port 30333 --ws-port 9944 --rpc-port 9934 --validator --node-key 0000000000000000000000000000000000000000000000000000000000000002  --execution Native \
   --reserved-node /ip4/54.176.87.85/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp /ip4/52.76.105.188/tcp/30333/p2p/12D3KooWHdiAxVd8uMQR1hGWXccidmfCwLqcMpGwR6QcTP6QRMuD -l peerset=debug
