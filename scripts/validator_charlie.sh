7da1f26553b7e5f13605540df708c7e0ff54cff7
Gautham J <Gauthamastro@gmail.com>
2020-11-28 20:25:37 +0530

    Removed Bootnodes from scripts

--- scripts/validator_charlie.sh
+++ scripts/validator_charlie.sh
@@ -1,2 +1,2 @@
 rm -r -f /tmp/bob/
-../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --charlie --port 30333 --ws-port 9945 --rpc-port 9934 --validator --node-key 0000000000000000000000000000000000000000000000000000000000000003 --bootnodes /ip4/217.182.197.118/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --charlie --port 30333 --ws-port 9945 --rpc-port 9934 --validator --node-key 0000000000000000000000000000000000000000000000000000000000000003
