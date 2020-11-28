0bdec05b26f36f88f309a430f9bd1af3edff44b4
Gautham J <Gauthamastro@gmail.com>
2020-11-28 15:58:16 +0530

    Updated IP for Charlie too

--- scripts/validator_charlie.sh
+++ scripts/validator_charlie.sh
@@ -7,4 +7,4 @@
   --rpc-port 9934 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000003 \
-  --bootnodes /ip4/54.176.87.85/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+  --bootnodes /ip4/217.182.197.118/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
