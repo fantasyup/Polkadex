2da62aa7a6d4088f428ff429604bdbaadbb7b797
Gautham J <Gauthamastro@gmail.com>
2020-11-28 17:52:23 +0530

    Something Happened with Script

--- scripts/bootnode_alice.sh
+++ scripts/bootnode_alice.sh
@@ -1,9 +1,1 @@
-../target/release/node-polkadex \
-  --base-path /tmp/alice \
-  --chain customSpecRaw.json \
-  --alice \
-  --port 30333 \
-  --ws-port 9944 \
-  --node-key 0000000000000000000000000000000000000000000000000000000000000001 \
-  --validator \
-  --execution Native
+../target/release/node-polkadex --base-path /tmp/alice --chain customSpecRaw.json --alice --port 30333 --ws-port 9944 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --validator --execution Native
--- scripts/validator_eve.sh
+++ scripts/validator_eve.sh
@@ -1,11 +1,1 @@
-../target/release/node-polkadex \
-  --base-path /tmp/bob \
-  --chain customSpecRaw.json \
-  --eve \
-  --port 30334 \
-  --ws-port 9945 \
-  --rpc-port 9934 \
-  --validator \
-  --node-key 0000000000000000000000000000000000000000000000000000000000000005 \
-  --bootnodes /ip4/217.182.197.118/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp \
-  --execution Native
+../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --eve --port 30334 --ws-port 9945 --rpc-port 9934 --validator --node-key 0000000000000000000000000000000000000000000000000000000000000005 --bootnodes /ip4/217.182.197.118/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp --execution Native
