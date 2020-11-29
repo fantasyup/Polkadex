fcc7d14b9e3f3d056599e73252cdc435440bd1e8
Gautham J <Gauthamastro@gmail.com>
2020-11-29 09:58:26 +0530

    Added Network Logging

--- scripts/validator_bob.sh
+++ scripts/validator_bob.sh
@@ -1,3 +1,3 @@
 rm -r -f /tmp/bob/
 ../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --bob --port 30333 --ws-port 9944 --rpc-port 9934 --validator --node-key 0000000000000000000000000000000000000000000000000000000000000002  --execution Native \
-  --reserved-nodes /ip4/54.176.87.85/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp /ip4/18.198.113.243/tcp/30333/p2p/12D3KooWSCufgHzV4fCwRijfH2k3abrpAJxTKxEvN1FDuRXA2U9x -l peerset=debug
+ -l sc_network=debug
--- scripts/validator_charlie.sh
+++ scripts/validator_charlie.sh
@@ -1,3 +1,3 @@
 rm -r -f /tmp/bob/
 ../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --charlie --validator --node-key 0000000000000000000000000000000000000000000000000000000000000003 --execution Native \
-  --reserved-nodes /ip4/52.76.105.188/tcp/30333/p2p/12D3KooWHdiAxVd8uMQR1hGWXccidmfCwLqcMpGwR6QcTP6QRMuD /ip4/54.176.87.85/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp -l peerset=debug
+ -l sc_network=debug
