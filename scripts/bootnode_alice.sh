74edf99de93fd04895a3e22be2a48ded1355825a
Gautham J <Gauthamastro@gmail.com>
2020-11-29 12:43:42 +0530

    Modified the Scripts

--- scripts/bootnode_alice.sh
+++ scripts/bootnode_alice.sh
@@ -1,3 +1,2 @@
 rm -r -f /tmp/alice
-../target/release/node-polkadex --base-path /tmp/alice --chain customSpecRaw.json --alice --port 30333 --ws-port 9945 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --validator --execution Native \
- -l sc_network=debug
+../target/release/node-polkadex --base-path /tmp/alice --chain customSpecRaw.json --alice --port 30333 --ws-port 9945 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --validator
